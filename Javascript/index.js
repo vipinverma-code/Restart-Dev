761a40ef6e61abd6d50fef19a9c623fd0d4d8cc1
vipinverma <vipinverma4224@gmail.com>
2026-07-09 23:50:27 +0530

    changes done

---
+++ Javascript/index.js
@@ -0,0 +1,4 @@
+// js ka code vs code me execute krne ke liye hme run time environment ki need hoti hai vo node.js provide krta hai.
+console.log(" I am a frontend developer");
+// for showing output of js code in browser.
+document.write("I am working as a frontend developer ");
