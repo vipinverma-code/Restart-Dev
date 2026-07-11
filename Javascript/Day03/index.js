2a4977cb894a905063a9b8cedb281a178ef88b52
vipinverma <vipinverma4224@gmail.com>
2026-07-12 02:37:35 +0530

    changes done

--- Javascript/Day03/index.js
+++ Javascript/Day03/index.js
@@ -13,6 +13,25 @@ let  x=10;
 let  y=10;
 x=x+y;
 console.log(x);
+// comparison operator
 
+let m=20;
+let n=10;
+console.log(m>n);
+console.log(m>=y);
+console.log(m<y);
+console.log(m==n);
+console.log(m===n);
+
+let z="10";
+let a = Number(z);
+console.log(a);
+console.log(typeof a);
+
+let c="121ac";
+let b= Number(c);
+// console.log(c);
+console.log(typeof (b));
+console.log(0/0);
 
 
