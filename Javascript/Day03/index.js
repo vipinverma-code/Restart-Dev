8b370c6776119a54d8d7ec85643e9e17960b513e
vipinverma <vipinverma4224@gmail.com>
2026-07-11 23:28:45 +0530

    changes done

---
+++ Javascript/Day03/index.js
@@ -0,0 +1,18 @@
+// operator
+// Arithmetic
+console.log(2+3);
+console.log(2-5);
+console.log(2*5);
+console.log(6/2);
+console.log(5%2);
+// Remainder
+console.log(5**2);
+// Assignment operator
+
+let  x=10;
+let  y=10;
+x=x+y;
+console.log(x);
+
+
+
