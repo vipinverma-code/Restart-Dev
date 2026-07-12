7b5817251ce53e6fc4741e26b4c1f33664408f84
vipinverma <vipinverma4224@gmail.com>
2026-07-13 00:08:31 +0530

    changes done

--- Javascript/Day03/Day05/index.js
+++ Javascript/Day03/Day05/index.js
@@ -1,4 +1,5 @@
 // Number
+// toFixed method
 let a=10;
 let b=345.6821
 console.log(a);
@@ -7,3 +8,8 @@ c=b.toFixed(3);
 console.log(c);
 let x=2.46;
 console.log(x.toFixed(1));
+
+// toPrecision Method
+console.log(b.toPrecision(50));
+console.log(b.toString());
+console.log(typeof (b.toString()));
