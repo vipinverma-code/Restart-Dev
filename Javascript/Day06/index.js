1070f6536768788447806bc100111e427b17aece
vipinverma <vipinverma4224@gmail.com>
2026-07-14 00:59:02 +0530

    changes done

---
+++ Javascript/Day06/index.js
@@ -0,0 +1,9 @@
+// Strings and Dates in Js
+// ways to create strings
+
+const str1="Vipin";
+const str2='Vipin Verma';
+const date=15;
+// with the use of backtick it prefers most because it doesnot give errors when it starts to new line.
+const str3=`You are a frontend developer recently ${date}`;
+console.log(str1, str2,str3);
