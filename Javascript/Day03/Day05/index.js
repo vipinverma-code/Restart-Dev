d368f57a5b3ba64555afcb9518f7478add71f981
vipinverma <vipinverma4224@gmail.com>
2026-07-13 20:23:13 +0530

    changes done

--- Javascript/Day03/Day05/index.js
+++ Javascript/Day03/Day05/index.js
@@ -1,15 +1,56 @@
 // Number
 // toFixed method
-let a=10;
-let b=345.6821
-console.log(a);
-console.log(b);
-c=b.toFixed(3);
-console.log(c);
-let x=2.46;
-console.log(x.toFixed(1));
+// let a=10;
+// let b=345.6821
+// console.log(a);
+// console.log(b);
+// c=b.toFixed(3);
+// console.log(c);
+// let x=2.46;
+// console.log(x.toFixed(1));
 
 // toPrecision Method
-console.log(b.toPrecision(50));
-console.log(b.toString());
-console.log(typeof (b.toString()));
+// console.log(b.toPrecision(50));
+// console.log(b.toString());
+// console.log(typeof (b.toString()));
+// let a= new Number(20);
+// console.log(a);
+// console.log(typeof a);
+// object reference ke based per comparison hota hai.
+console.log(Boolean(null));
+
+// Non Primitive data reference ke basis per compare hote hai.
+// Primitive: copy by value
+
+// let a=10;
+// let b=a;
+// console.log(a==b);
+
+// Math object ke built-in methods
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.ceil(2.4));
+// console.log(Math.max(20,50,63));
+// console.log(Math.random());
+
+// 0-9 ke beech random number generate krana hai
+
+// console.log(Math.floor(Math.random()*10)+1);
+// 15-25
+// formula:- Math.floor(Math.random()*(max-min+1)+min
+
+console.log(Math.floor((Math.random()*11)+15));
+
+// used for otp generate
+// 1000-9999
+console.log(Math.floor(Math.random()*(9999-1000 +1)+1000));
+// function to generate random number
+function random() {
+    for(let i=0; i<0.99999;i++){
+        console.log(i);
+    } 
+}
+random();
+
+
