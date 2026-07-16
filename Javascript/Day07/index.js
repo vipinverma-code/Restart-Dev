11c783ba9ad59488a2408ec1ae3cf4c0a9a85a3d
vipinverma <vipinverma4224@gmail.com>
2026-07-16 21:41:08 +0530

    chages done

--- Javascript/Day07/index.js
+++ Javascript/Day07/index.js
@@ -3,9 +3,20 @@
 // let marks2=50;
 // let marks3=70;
 // let marks4=80;
-let marks=[100,50,70,80,90];
-console.log(marks);
+// let marks=[100,50,70,80,90];
+// console.log(marks);
+// Array can store heterogenous type of data
 const arr=[100,30,"Rohit",true];
 console.log(arr);
 arr[0]="Vipin";
+console.log(arr);
+// console.log(marks.length);
+console.log(typeof arr);
+// Access method of array 
+console.log(arr[2]);
+// Adding new element to array in the end
+arr.push(90);
+console.log(arr);
+// Remove element from the end of array
+arr.pop();
 console.log(arr);
