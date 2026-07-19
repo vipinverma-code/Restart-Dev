01662ee7c902205b1ca0815167475f798ad9e60e
vipinverma <vipinverma4224@gmail.com>
2026-07-19 20:56:17 +0530

    chnages done

---
+++ Javascript/Day08/index.js
@@ -0,0 +1,26 @@
+// Object is a collection of key-value pairs
+// key value
+const user={
+    name:"Vipin",
+    age:20,
+    emailId:"vipinverma@gmail.com",
+    amount:350000,
+    "home address":"lmp"
+}
+// console.log(user);
+// console.log(typeof user);
+// // for accessing the elements of object with their key value
+// console.log(user.name);
+console.log(user["name"]);
+console.log(user["amount"]);
+console.log(user["home address"]);
+
+// CRUD operation: Create read Update ANd Delete
+
+// update
+// user.aadhar=458309741918;
+// user.amount=50000000000;
+
+// delete
+// delete user.emailId;
+// console.log(user);
