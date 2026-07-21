a20df293c7e38592aa45d81056b206edb13fbe49
vipinverma <vipinverma4224@gmail.com>
2026-07-21 22:43:11 +0530

    changes done

--- Javascript/Day08/index.js
+++ Javascript/Day08/index.js
@@ -40,13 +40,13 @@
 //     console.log(keys,user[keys]);
 // }
 
-const user={
-    name:"Vipin",
-    age:20,
-    emailId:"vipinverma@gmail.com",
-    amount:350000,
-    "home address":"lmp"
-}
+// const user={
+//     name:"Vipin",
+//     age:20,
+//     emailId:"vipinverma@gmail.com",
+//     amount:350000,
+//     "home address":"lmp"
+// }
 
 // const name=user.name;;
 // const age=user.age;
@@ -54,18 +54,107 @@ const user={
 // console.log(age);
 
 // Destructuring of Object
-const {name,age,amount,}=user;
-console.log(name,age,amount);
-// Destucturing of array
-const arr=[10,20,30,40];
-const [first,second]=arr;
-console.log(first,second);
+// const {name,age,amount,}=user;
+// console.log(name,age,amount);
+// // Destucturing of array
+// const arr=[10,20,30,40];
+// const [first,second]=arr;
+// console.log(first,second);
 // use of for of loop to find the key and values of Object
 // for of loop array per lgta hai.
-for(let keys of Object.keys(user)){
-   console.log(keys);
+// for(let keys of Object.keys(user)){
+//    console.log(keys);
+// }
+
+// for(let entries of Object.entries(user)){
+//     console.log(entries);
+// }
+// function: independent scope
+// function sum(a,b){
+//     return a+b;
+// }
+// const result=sum(2,3);
+// console.log(result);
+// method: inside the object
+// const role={
+//     name:"Vipin Verma",
+//     profile:"Frontend Developer",
+//     experience:"6 months",
+//     education:"b.tech",
+//     greet:function(){
+//         // console.log("I am trying to crack the opportunities");
+//         return "lets try to crack";
+//     }
+// }
+// const wish=role.greet();
+// console.log(wish);
+// use of this keyword
+// const user1={
+//         name:"Vipin Verma",
+//     profile:"Frontend Developer",
+//     experience:"6 months",
+//     education:"b.tech",
+//     greet:function(){
+//         // console.log("I am trying to crack the opportunities");
+//         // return "lets try to crack";
+//         console.log(this.profile);
+//     }
+// }
+// user1.greet();
+
+// const user2={
+//         name:"Sachin Verma",
+//     profile:"Frontend Developer",
+//     experience:"6 months",
+//     education:"b.tech",
+//     greet:function(){
+//         // console.log("I am trying to crack the opportunities");
+//         // return "lets try to crack";
+//         console.log(this.education);
+//     }
+// }
+// user2.greet();
+
+// create object inside object:nested object
+
+const student={
+    name:"Vipin Verma",
+    age:"22",
+    // object inside object
+    address:{
+        city:"lmp",
+        state:"uttar pradesh",
+        village:"mohaddipur"
+    }
+}
+// access the nested object value
+// console.log(student.address.city);
+// add property inside nested object
+// student.address.pincode=262701;
+// console.log(student);
+// update
+// student.address.city="lko";
+// console.log(student);
+// delete
+// delete student.address.village;
+// console.log(student);
+
+// shallow copy
+const person={
+    name:"Vipin Verma",
+    age:"22",
+    // object inside object
+    address:{
+        city:"lmp",
+        state:"uttar pradesh",
+        village:"mohaddipur"
+    }
 }
 
-for(let keys of Object.entries(user)){
-    console.log(keys);
-}
+const person2={...person};
+// with the spread operator hm dono ki independent copy bna rhe hai
+person2.name="ankit";
+person2.address.city="allahabad";
+console.log(person2);
+console.log(person);
+ 
