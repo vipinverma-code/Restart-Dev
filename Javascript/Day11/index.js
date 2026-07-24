d886494f51cfbfae1b28944ebb4f0365df554ae0
vipinverma <vipinverma4224@gmail.com>
2026-07-24 22:16:26 +0530

    changes done

--- Javascript/Day11/index.js
+++ Javascript/Day11/index.js
@@ -1,21 +1,56 @@
 // Scope and Closure, Higher Order function
-let a =10;
-const b=20;
-var e=90;
+// let a =10;
+// const b=20;
+// var e=90;
 // a and b have global scope
+// if(true){
+//     console.log(b);
+//     var e=90;
+// }
+// console.log(e);
 
-if(true){
-    console.log(b);
-    var e=90;
-    // console.log(e);
-}
-console.log(e);
-
-console.log(a);
-function greet(){
-    let c=30; 
-    console.log(a);
-}
-greet();
+// console.log(a);
+// function greet(){
+//     let c=30; 
+//     console.log(a);
+// }
+// greet();
 // console.log(c);
 // var c have functional scope
+
+// let global = 30;
+// function greet(){
+//     let global=40;
+//     console.log(global);
+// }
+// greet();
+
+
+// let global=30;
+// function greet(){
+//     let global=40;
+//     function meet(){
+//         console.log(global);
+//     }
+//     meet();
+// }
+// greet();
+function createCounter(){
+    let count= 0;
+    function increment(){
+        count++;
+           console.log("I am increment function");
+        return count;
+        // console.log("I am increment function");
+    }
+      return increment;
+}
+
+// console.log(count);
+const counter=createCounter();
+console.log(counter());
+// count();
+
+
+
+
