2ffa36c211c83ccb7d79821f1c66f14ec8fb8bfc
vipinverma <vipinverma4224@gmail.com>
2026-07-26 19:06:26 +0530

    changes done

---
+++ Javascript/Day12/index.js
@@ -0,0 +1,14 @@
+// forEach, filter, reducer, map,set
+const arr=[10,20,30,"Rohit",90];
+// arr.forEach((number,index,arr)=>{
+//     console.log(number,index,arr);
+// })
+// let sum=0;
+// arr.forEach((num)=>{
+//     sum+=num;
+   
+// })
+//  console.log(sum);
+
+
+// filter
