b413a616edb07610802317b6d512935ecb21a6e8
vipinverma <vipinverma4224@gmail.com>
2026-08-02 00:18:50 +0530

    changes done

--- Javascript/Day14/first.js
+++ Javascript/Day14/first.js
@@ -69,12 +69,39 @@ const arr = ["Milk","Halwa","Paneer","tofu","Tea"];
 // parent ko select krna jnha ye list ke items daalne hai ;
 const unorderedELement=document.getElementById("listing");
 const fragment=document.createDocumentFragment();
+
 for(let food of arr){
     const list=document.createElement("li");
     list.textContent= food;
     // unorderedELement.append(list);
     // optimizecd code 
     fragment.appendChild(list);
+//   const arr1=[...list];
     
 }
 unorderedELement.append(fragment);
+
+
+// now we learn how to delete an element
+// phle us element ko select krte useImperativeHandle(
+
+const del=document.getElementById("first");
+del.remove();
+
+
+// create the element
+const marketing=document.createElement("p");
+marketing.textContent="marketing is a good space to excel";
+marketing.id="mnc";
+console.log(marketing);
+// now show on ui , so select the element , if html code have no element only body
+document.body.append(marketing);
+
+
+
+// now learn about the older syntax
+
+const months=document.getElementById("month");
+console.log(months.children);
+console.log(months.childNodes);
+
