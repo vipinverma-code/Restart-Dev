012502a3513662a4ada9b99ef126f1642d2461b2
vipinverma <vipinverma4224@gmail.com>
2026-08-01 22:02:46 +0530

    changes done

--- Javascript/Day14/first.js
+++ Javascript/Day14/first.js
@@ -24,3 +24,43 @@ element2.after(anotherNewElement);
 
 // change the font size of anotherNewElement
 anotherNewElement.style.fontSize= "25px";
+
+
+// creation of new element
+const newElement2=document.createElement('h3');
+newElement2.innerText="Rakshabandhan aa rha hai";
+newElement2.id="fourth";
+// newElement2.className="diwali";
+// newElement2.className="Holi";
+newElement2.classList.add("diwali");
+newElement2.classList.add("Holi");
+newElement2.style.backgroundColor="brown";
+newElement2.style.fontSize = "20px";
+newElement2.setAttribute("Hello","ji");
+console.log(newElement2);
+// Selection of element
+const element4=document.getElementById("third");
+element4.after(newElement2);
+console.log(newElement2.getAttribute("id")); 
+console.log(newElement2.getAttribute("class")); 
+console.log(newElement2.getAttribute("src")); 
+console.log(newElement2.getAttribute("Hello")); 
+
+// 
+const list = document.createElement("li");
+list.textContent = "Milk";
+const list2 = document.createElement("li");
+list2.textContent="Banana";
+const list3 = document.createElement("li");
+list3.textContent="Apple";
+const list4 = document.createElement("li");
+list4.textContent="Paneer";
+
+const unorderedelement=document.getElementById("listing");
+unorderedelement.append(list);
+unorderedelement.append(list2);
+unorderedelement.prepend(list3);
+// paneer milk ke baad aaye
+// list.after(list4);
+console.log(unorderedelement.children[1].after(list4));
+
