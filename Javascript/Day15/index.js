7cd080a56188160fd7eb44f2c6dd03ee9f03c71c
vipinverma <vipinverma4224@gmail.com>
2026-08-04 17:23:54 +0530

    chnages done

--- Javascript/Day15/index.js
+++ Javascript/Day15/index.js
@@ -4,23 +4,48 @@
 // }
 
 
-const element = document.getElementById("second");
+// const element = document.getElementById("second");
 // element.onclick=function handleClick(){
 //     element.textContent="Try to rebuild yourself";
 // }
 
-element.addEventListener('click',()=>{
-    element.textContent = "Strike is Coming";
-});
-element.addEventListener('click',()=>{
-    element.style.backgroundColor="brown";
-});
+// element.addEventListener('click',()=>{
+//     element.textContent = "Strike is Coming";
+// });
+// element.addEventListener('click',()=>{
+//     element.style.backgroundColor="brown";
+// });
 
-let a ={
-    greet:function(value){
-        console.log("Hello", value);
-    }
-}
-a.greet(30);
-a.greet(40);
+// let a ={
+//     greet:function(value){
+//         console.log("Hello", value);
+//     }
+// }
+// a.greet(30);
+// a.greet(40);
+
+const element2=document.getElementById("mess");
+element2.addEventListener('dblclick', ()=>{
+    element2.textContent="Let's focus on one thing at a time";
+});
+console.log(element2);
+
+// const element=document.getElementById("child1");
+// element.addEventListener('click',()=>{
+//     element.textContent="It is clicked";
+// });
+
+// const element1=document.getElementById("child2");
+// element1.addEventListener('click',()=>{
+//     element1.textContent="It is clicked";
+// });
+
+// here i use querySelectorAll to all elements for apply event
+
+const childs = document.querySelectorAll(".child");
+childs.forEach((child)=>{
+    child.addEventListener('click',()=>{
+        child.textContent="I'm clicked";
+    });
+});
 
