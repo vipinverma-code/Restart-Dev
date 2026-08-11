1359c5e763e01d167b0f972b1b5ed6f06e682910
vipinverma <vipinverma4224@gmail.com>
2026-08-12 03:32:06 +0530

    changes done

---
+++ Javascript/Day17/first.js
@@ -0,0 +1,76 @@
+// console.log("First");
+// let sum = 0;
+// for(let i=0;i<3000000000;i++)
+//     sum+=i;
+
+// console.log(sum);
+// console.log("Last");
+// console.log("Hello Ji");
+// setTimeout(() => {
+//     console.log("Time Out Executed");
+// }, 5000);
+
+// console.log("I am the end");
+
+// console.log("Hi Ji");
+
+// const button1 = document.getElementById("button1");
+// button1.addEventListener('click',()=>{
+//     console.log("Button 1 is clicked");
+// });
+
+
+// const button2 = document.getElementById("button2");
+// button2.addEventListener('click',()=>{
+//     console.log("Button 2 is clicked");
+// });
+
+// const button3 = document.getElementById("button3");
+// button3.addEventListener('click',()=>{
+//     console.log("Button 3 is clicked");
+// });
+
+// console.log("I am the end");
+
+
+
+// console.log("I am first");
+
+// setTimeout(()=>{
+//     console.log("I am timeOut");
+// },5000);
+
+
+// console.log("I am end");
+
+
+
+
+
+console.log("Start the Operation");
+
+fetch("https://api.github.com/users").then(()=>{
+    console.log("Git Hub user info");
+})
+
+console.log("end of operation");
+
+
+let arr = [100,20,90,70];
+
+// arr.push(10);
+let newarr=arr.map((x)=>{
+   return  x*10;
+});
+console.log(newarr);
+
+// function hello(){
+//     console.log("Hi ji");
+// }
+
+
+// hello();
+// console.log("Hello Ji");
+
+// ahsdjsah("Hello Ji");
+
---
+++ Javascript/Day18/first.js
@@ -0,0 +1,53 @@
+// zomato application
+const orderDetail= {
+orderId: 123123,
+food:["Pizza","biryani","coke"],
+cost:620,
+customer_name: "Rohit",
+customer_location: "Dwarka",
+restaurant_location: "Delhi"
+}
+
+function placedOrder(orderDetail, Callback){
+    console.log(`${orderDetail.cost} Payment is in progress`);
+    console.log("Payment is in progress");
+    setTimeout(()=>{
+        console.log("Payment is recieved and order get placed ");
+        Callback();
+    },3000)
+}
+// now next funtion for preparing food
+function preparingOrder(Callback){
+    console.log("Your food preparation started");
+    setTimeout(()=>{
+        console.log("Your order is now prepared");
+        Callback();
+    },3000);
+}
+
+function pickupOrder(Callback){
+    console.log("Delivery boy is on way to pickup order ");
+    setTimeout(()=>{
+        console.log("I have picked up the order");
+        Callback();
+    },3000);
+}
+
+function deliverOrder(){
+    console.log("I am on my way to deliver order");
+    setTimeout(()=>{
+        console.log("Order deliverd successfully");
+    },3000)
+}
+
+
+// now calling the function 
+placedOrder(orderDetail,()=>{
+    preparingOrder(()=>{
+        pickupOrder(()=>{
+            deliverOrder(()=>{
+            })
+        })
+    })
+});
+
