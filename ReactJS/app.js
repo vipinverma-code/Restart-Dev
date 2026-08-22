6cfc8da52b0a42c561ff836f36f4c75ee036701f
vipinverma <vipinverma4224@gmail.com>
2026-08-22 22:54:11 +0530

    changes done

--- ReactJS/app.js
+++ ReactJS/app.js
@@ -2,24 +2,30 @@
 //     className:"element",
 //     id:"first"
 // }
-
-const React={
-    createElement:function(tag,attributes,children){
-        const element= document.createElement(tag);
-   element.textContent= children;
-   for(const key in attributes){
-    if(key==='style'){
-        Object.assign(element.style, attributes.style);
-    }
-    else{
-        element[key] = attributes[key];
-    }   
-}
-return element;
-    }
-}
+// custom react
+// const React={
+//     // createElement is a method
+//     createElement:function(tag,attributes,children){
+//         const element= document.createElement(tag);
+//    element.textContent= children;
+//    for(const key in attributes){
+//     if(key==='style'){
+//         Object.assign(element.style, attributes.style);
+//     }
+//     else{
+//         element[key] = attributes[key];
+//     }   
+// }
+// return element;
+//     }
+// }
+// const ReactDOM = {
+//     render:function(child,parent){
+//         parent.append(child);
+//     }
+// }
 const element1=React.createElement("h1",{className:"element",id:"first",style:{fontSize:"30px",backgroundColor:"orange",color:"white"}},"Hello Vipin?");
-const element2=React.createElement("p",{"id":"para","class":"paragraph",style:{fontSize:"20px",backgroundColor:"pink", color:"green"}},"Hello , Vipin , you are a good developer");
+const element2=React.createElement("p",{"id":"para","className":"paragraph",style:{fontSize:"20px",backgroundColor:"pink", color:"green"}},"Hello , Vipin , you are a good developer");
 // create a h1 element using js
 // const element1=document.createElement("h1");
 // element1.textContent="Coder Army";
@@ -38,7 +44,11 @@ const element2=React.createElement("p",{"id":"para","class":"paragraph",style:{f
 // element2.style.color="green";
 // Now i have to connect this element with the dom
 const root= document.getElementById('root');
-root.append(element1);
-root.append(element2);
+// ReactDOM.render(element1,root);
+ReactDOM.render(element2,root);
+// root.append(element1);
+// root.append(element2);
+// Create a parent element
+
 
 
