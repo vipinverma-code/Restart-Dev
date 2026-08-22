f27d86e935a853dae483558a314486ea4e31d58c
vipinverma <vipinverma4224@gmail.com>
2026-08-22 23:58:32 +0530

    changes done

--- ReactJS/app.js
+++ ReactJS/app.js
@@ -24,8 +24,31 @@
 //         parent.append(child);
 //     }
 // }
+
+// Original React
+const React={
+    createElement: function(type,props,children){
+        return{
+            type:type,
+            props:{
+                ...props,
+                children:children
+            }
+        }
+    }
+}
+
 const element1=React.createElement("h1",{className:"element",id:"first",style:{fontSize:"30px",backgroundColor:"orange",color:"white"}},"Hello Vipin?");
 const element2=React.createElement("p",{"id":"para","className":"paragraph",style:{fontSize:"20px",backgroundColor:"pink", color:"green"}},"Hello , Vipin , you are a good developer");
+
+console.log(element1);
+
+
+
+
+
+
+
 // create a h1 element using js
 // const element1=document.createElement("h1");
 // element1.textContent="Coder Army";
@@ -45,7 +68,7 @@ const element2=React.createElement("p",{"id":"para","className":"paragraph",styl
 // Now i have to connect this element with the dom
 const root= document.getElementById('root');
 // ReactDOM.render(element1,root);
-ReactDOM.render(element2,root);
+// ReactDOM.render(element2,root);
 // root.append(element1);
 // root.append(element2);
 // Create a parent element
