13b7390d6459e4d0d375446e188bd747ca30076c
vipinverma <vipinverma4224@gmail.com>
2026-08-27 14:09:18 +0530

    changes done

---
+++ ReactJS/Day02/app.js
@@ -0,0 +1,8 @@
+const element = React.createElement('h1',{id:'title'},"Hello Coder Army");
+const root= ReactDOM.createRoot(document.getElementById('root'));
+root.render(element);
+// creation of an element in the reactjs
+
+const paragraph= React.createElement('p',{id:"para"},"I am currently giving everything to become a frontend developer");
+const para = ReactDOM.createRoot(document.getElementById('root'));
+root.render(para);
