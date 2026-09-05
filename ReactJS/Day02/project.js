6565875c109b924d94eb29056c2a96ec1012334f
vipinverma <vipinverma4224@gmail.com>
2026-09-05 15:02:36 +0530

    changes done

--- ReactJS/Day02/project.js
+++ ReactJS/Day02/project.js
@@ -1,13 +1,32 @@
-function Header(){
+function Header({name}){
     return(
-        <h1>Welcome to Indian Election Commission</h1>
+        <h1>{name} Welcome to Indian Election Commission Website</h1>
     )
 }
-
+function Main({user}){
+    return(
+        <>
+        <h1>Hi{user.name}</h1>
+        <h2>{user.age>18? "You are elegible to vote":"not eleigible"}</h2>
+        <h3>You aur from{user.city}</h3>
+        </>
+  
+    )
+}
+function Footer(){
+    return(
+        <h3>Thanks to all users for visiting our website</h3>
+    )
+}
 function App(){
     return(
-        <Header></Header>
+        <>
+        <Header name="Vipin"></Header>
+        <Main user={{ name:"Vipin",age:22,city:"LMP" }} />
+        <Footer/>
+        </>
     )
 }
 const root= ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
+
