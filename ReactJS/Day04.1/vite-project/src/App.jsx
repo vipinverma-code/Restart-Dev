4ca87ec734e1001e5b6ec01d665da326dd85b8d3
vipinverma <vipinverma4224@gmail.com>
2026-09-07 12:46:21 +0530

    chnges done

--- ReactJS/Day04.1/vite-project/src/App.jsx
+++ ReactJS/Day04.1/vite-project/src/App.jsx
@@ -1,3 +1,16 @@
-function App(){
-    let count=0;
+import { useState } from "react";
+export default function App(){
+    let [count,setCount]=useState(0);
+
+    function IncreaseCount(){
+        count++;
+        setCount(count);
+    }
+    return (
+      <>
+        <h1>Counter:{count}</h1>
+        <button style={{"fontSize":"15px"}} onClick={IncreaseCount}>Increment:{count}</button>
+        
+      </>
+    );
 }
