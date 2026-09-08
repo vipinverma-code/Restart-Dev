1778162f45c4dabe25169754641eecadfe9e2935
vipinverma <vipinverma4224@gmail.com>
2026-09-09 03:07:15 +0530

    changes done

---
+++ ReactJS/Day06/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+    return (
+      <>
+        <p>This is the counter for react App </p>
+        <h1>Counter:</h1>
+        <button>Increment</button>
+        <button>Decrement</button>
+      </>
+    );
+}
+export default App;
