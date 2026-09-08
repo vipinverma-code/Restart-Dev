8a2d58266be275f6731f978accd859f8d90ee97f
vipinverma <vipinverma4224@gmail.com>
2026-09-08 21:40:29 +0530

    changes done

--- ReactJS/Day05/use of useEffect/src/Clock.jsx
+++ ReactJS/Day05/use of useEffect/src/Clock.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
 function Clock(){
     const [time,setTime] = useState(new Date().toLocaleTimeString());
+    const [show,setShow] = useState(true);
+    useEffect(()=>{
+           setInterval(() => {
+             setTime(new Date().toLocaleString());
+             console.log("hello");
+           }, 1000);
+
+    },[])
+    // here in useEffect [], it is a empty dependency array.
+
+    // setInterval(()=>{
+    //     setTime(new Date().toLocaleString());
+    //     console.log("hello");
+    // },1000)
     return(
         <>
-        <h1>Current Time:</h1>
+        <h1>Current Time:{time} </h1>
+        <button onClick={()=>setShow(!show)}>{{show?"hide":"show"}}</button>
         </>
     )
 }
